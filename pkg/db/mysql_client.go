8f8c7725ff9e7ccd0783f66ae0d53898909a0390
cat <cat@github.com>
2023-09-06 09:43:46 +0800

    [tools]: create database

--- pkg/db/mysql_client.go
+++ pkg/db/mysql_client.go
@@ -1,8 +1,13 @@
 package db
 
 import (
+	"bufio"
 	"database/sql"
+	"errors"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/TOMO-CAT/UserManagementSystem/pkg/config"
@@ -15,56 +20,16 @@ import (
 var PtrMysqlClient *mysqlClient
 
 type mysqlClient struct {
-	*gorm.DB
+	*gorm.DB         // gorm sql 接口, 可以直接继承其方法
+	rawDB    *sql.DB // 原生的 sql 接口
 }
 
 func (c *mysqlClient) Init() (err error) {
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
-		config.GlobalUmsConfig.Mysql.User,
-		config.GlobalUmsConfig.Mysql.Password,
-		config.GlobalUmsConfig.Mysql.IP,
-		config.GlobalUmsConfig.Mysql.Port,
-		config.GlobalUmsConfig.Mysql.Database,
-	)
-
-	if c.DB, err = gorm.Open(mysql.Open(dataSourceName), &gorm.Config{}); err != nil {
-		return
-	}
-
-	// 设置连接池
-	var sqlDB *sql.DB
-	if sqlDB, err = c.DB.DB(); err != nil {
-		logger.Error("fetch mysql db fail with err [%v]", err)
-		return
-	}
-	sqlDB.SetMaxOpenConns(config.GlobalUmsConfig.Mysql.MaxOpenConns)
-	sqlDB.SetMaxIdleConns(config.GlobalUmsConfig.Mysql.MaxIdleConns)
-	sqlDB.SetConnMaxLifetime(time.Hour)
-	return nil
+	return c.init(config.GlobalUmsConfig.Mysql.Database)
 }
 
 func (c *mysqlClient) InitWithoutDatabase() (err error) {
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%d)/",
-		config.GlobalUmsConfig.Mysql.User,
-		config.GlobalUmsConfig.Mysql.Password,
-		config.GlobalUmsConfig.Mysql.IP,
-		config.GlobalUmsConfig.Mysql.Port,
-	)
-
-	if c.DB, err = gorm.Open(mysql.Open(dataSourceName), &gorm.Config{}); err != nil {
-		return
-	}
-
-	// 设置连接池
-	var sqlDB *sql.DB
-	if sqlDB, err = c.DB.DB(); err != nil {
-		logger.Error("fetch mysql db fail with err [%v]", err)
-		return
-	}
-	sqlDB.SetMaxOpenConns(config.GlobalUmsConfig.Mysql.MaxOpenConns)
-	sqlDB.SetMaxIdleConns(config.GlobalUmsConfig.Mysql.MaxIdleConns)
-	sqlDB.SetConnMaxLifetime(time.Hour)
-	return nil
+	return c.init("")
 }
 
 func (c *mysqlClient) Close() {
@@ -76,18 +41,11 @@ func (c *mysqlClient) Close() {
 	}
 }
 
-func (c *mysqlClient) RawDB() (rawDB *sql.DB) {
-	var err error
-	if rawDB, err = c.DB.DB(); err != nil {
-		logger.Error("fetch mysql raw db fail with err [%v]", err)
-		panic(err)
-	}
-	return
-}
-
+// Query2InterfaceMap
+// TODO(cat): 让 Query2InterfaceMap 入参支持 Query2InterfaceMap("SELECT user_name from USER where id = ?", 100); 的 ? 写法
 func (c *mysqlClient) Query2InterfaceMap(sqlStr string) (res []map[string]interface{}, err error) {
 	var rows *sql.Rows
-	rows, err = c.RawDB().Query(sqlStr)
+	rows, err = c.Raw(sqlStr).Rows()
 	defer rows.Close()
 
 	if err != nil {
@@ -103,6 +61,178 @@ func (c *mysqlClient) Query2InterfaceMap(sqlStr string) (res []map[string]interf
 	}
 
 	return
+}
+
+func (c *mysqlClient) Query2StringMap(sqlStr string) (res []map[string]string, err error) {
+	var interfaceRes []map[string]interface{}
+	interfaceRes, err = c.Query2InterfaceMap(sqlStr)
+	if err != nil {
+		return
+	}
+
+	for _, record := range interfaceRes {
+		tmpMap := make(map[string]string)
+		for k, v := range record {
+			switch v.(type) {
+			case string:
+				tmpMap[k] = v.(string)
+			case []byte:
+				tmpMap[k] = string(v.([]byte))
+			case nil:
+				tmpMap[k] = ""
+			case int:
+				tmpMap[k] = strconv.Itoa(v.(int))
+			case float64:
+				tmpMap[k] = fmt.Sprintf("%f", v.(float64))
+			default:
+				logger.Error("Query2StringMap fail with invalid type||k=%s||v=%v||type=%T", k, v, v)
+				tmpMap[k] = ""
+				continue
+			}
+		}
+		res = append(res, tmpMap)
+	}
+	return
+}
+
+// Query2StructArray 查询 sql, 并将结果映射到 resPtr 指针指向的结构体中
+// @param sqlStr 待查询的 sql
+// @param resPtr目标结构体指针的数组 &[]struct
+// @return err
+//
+// eg:
+// var StudentInfo []struct {
+//     Name  string  `ddb:"name"`
+//     Score float64 `ddb:"score"`
+// }
+// sqlStr := "SELECT name, score FROM student"
+// err = PtrMySqlClient.Query2StructArray(sql, &StudentInfo)
+func (c *mysqlClient) Query2StructArray(sqlStr string, resPtr interface{}) (err error) {
+	var (
+		rows *sql.Rows
+	)
+	rows, err = c.Raw(sqlStr).Rows()
+	defer rows.Close()
+
+	if err != nil {
+		logger.Error("query fail||err=%v||sql=%s", err, sqlStr)
+		return
+	}
+
+	err = scanner.Scan(rows, resPtr)
+	if err != nil {
+		logger.Error("scan fail||err=%v||sql=%s", err, sqlStr)
+		return
+	}
+	return
+}
+
+// Query2InsertStatement 将查询结果转化成 insert 语句, 用于拷贝线上数据库
+// @param tableName 数据表名
+// @param sqlStr 查询 sql
+// @param outputFile 保存 insert 语句的文件
+// @return err
+//
+// eg:
+// @param sqlStr SELECT name, score FROM student WHERE id < 100;
+// @param tableName student
+// @param outputFile "./output.txt"
+//
+// 写入文件的内容:
+// INSERT INFO `student` (`name`, `score`) VALUES ("cat", 78), ("dog", 21);
+func (c *mysqlClient) Query2InsertStatement(tableName, sqlStr, outputFile string) (err error) {
+	// 判断文件是否存在, 不存在时创建
+	_, err = os.Stat(outputFile)
+	if os.IsNotExist(err) {
+		var f *os.File
+		if f, err = os.Create(outputFile); err != nil {
+			return
+		}
+		defer f.Close()
+	}
+
+	var queryRes []map[string]string
+	if queryRes, err = c.Query2StringMap(sqlStr); err != nil {
+		return
+	}
+	if len(queryRes) <= 0 {
+		return errors.New("empty res")
+	}
+
+	var f *os.File
+	if f, err = os.OpenFile(outputFile, os.O_WRONLY|os.O_APPEND, 0666); err != nil {
+		return
+	}
+	defer f.Close()
+
+	writer := bufio.NewWriter(f)
+
+	// 获取所有字段名
+	var fields []string
+	var fieldsWithBackquote []string
+	for field := range queryRes[0] {
+		fields = append(fields, field)
+		fieldsWithBackquote = append(fieldsWithBackquote, "`"+field+"`")
+	}
+
+	// 构造字段子句
+	// eg: (`name`, `score`)
+	fieldClause := strings.Join(fieldsWithBackquote, ",")
+
+	// 构造值子句列表
+	// eg: [("cat", 78), ("dog", 21)]
+	var valueClauseArray []string
+	for _, record := range queryRes {
+		var values []string
+		for _, field := range fields {
+			value := "\"" + record[field] + "\""
+			values = append(values, value)
+		}
+		valueClause := "(" + strings.Join(values, ",") + ")"
+		valueClauseArray = append(valueClauseArray, valueClause)
+	}
+	valueClauseArrayStr := strings.Join(valueClauseArray, ",\n")
+
+	// 组成 insert statement
+	// eg: INSERT INFO `student` (`name`, `score`) VALUES ("cat", 78), ("dog", 21);
+	insertStatementFmt := "INSERT INTO \n `%s` (%s) \n VALUES \n %s;"
+	insertStatement := fmt.Sprintf(insertStatementFmt, tableName, fieldClause, valueClauseArrayStr)
+	writer.WriteString(insertStatement)
+	writer.WriteString("\n")
+	writer.Flush()
+
+	return nil
+}
+
+func (c *mysqlClient) init(databaseName string) (err error) {
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s",
+		config.GlobalUmsConfig.Mysql.User,
+		config.GlobalUmsConfig.Mysql.Password,
+		config.GlobalUmsConfig.Mysql.IP,
+		config.GlobalUmsConfig.Mysql.Port,
+		databaseName,
+	)
+
+	if c.DB, err = gorm.Open(mysql.Open(dataSourceName), &gorm.Config{}); err != nil {
+		return
+	}
+
+	if c.rawDB, err = c.DB.DB(); err != nil {
+		logger.Error("fetch mysql db fail with err [%v]", err)
+		return
+	}
+
+	// 判断是否能 ping 通
+	if err = c.rawDB.Ping(); err != nil {
+		logger.Error("ping mysql fail with err [%v]", err)
+		return
+	}
+
+	// 设置连接池
+	c.rawDB.SetMaxOpenConns(config.GlobalUmsConfig.Mysql.MaxOpenConns)
+	c.rawDB.SetMaxIdleConns(config.GlobalUmsConfig.Mysql.MaxIdleConns)
+	c.rawDB.SetConnMaxLifetime(time.Hour)
+	return nil
 }
 
 func init() {
--- tools/init_mysql/init_mysql.go
+++ tools/init_mysql/init_mysql.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/TOMO-CAT/UserManagementSystem/pkg/config"
 	"github.com/TOMO-CAT/UserManagementSystem/pkg/db"
@@ -33,35 +32,58 @@ func main() {
 		panic(err)
 	}
 
-	createDatabase()
+	// 创建数据库
+	createDatabase(kDatabaseName)
 }
 
-func createDatabase() {
+func createDatabase(dbName string) {
+	var (
+		err error
+	)
 
-	// 检查数据库是否存在
-	if err := db.PtrMysqlClient.InitWithoutDatabase(); err != nil {
-		logger.Error("init mysql client fail with err [%v]", err)
-		panic(err)
+	// 初始化数据库
+	if err = db.PtrMysqlClient.InitWithoutDatabase(); err != nil {
+		logger.Fatal("init mysql client fail with err [%v]", err)
 	}
-	db.PtrMysqlClient.Close()
+	defer db.PtrMysqlClient.Close()
 	logger.Info("init mysql client without database successfully!")
 
-	if rawDB, err := db.PtrMysqlClient.RawDB(); err != nil {
-		logger.Error("get raw db from mysql client fail with err [%v]", err)
-		panic(err)
-	} else {
-		_, err = rawDB.Exec("USE ums;")
-		logger.Info("[use ums] %v", err)
-		if err != nil {
-			log.Fatal(err)
-		}
+	// 检查数据库是否存在
+	if isDatabaseExist(dbName) {
+		logger.Info("database [%s] already exist!", dbName)
+		return
+	}
+	logger.Info("database [%s] not exist, create it now!", dbName)
 
-		fmt.Println("Database [ums] exists")
+	// 创建数据库
+	if execResult := db.PtrMysqlClient.Exec(fmt.Sprintf("CREATE DATABASE %s;", dbName)); execResult.Error != nil {
+		logger.Fatal("crate database [%s] fail with err [%v]", dbName, execResult.Error)
+	} else if execResult.RowsAffected != 1 {
+		logger.Fatal("create database [%s] fail with RowsAffected [%d]", dbName, execResult.RowsAffected)
 	}
 
-	fmt.Println(db.PtrMysqlClient.Raw("SHOW DATABASES LIKE mysql"))
+	// 再次检查数据库是否创建成功
+	if !isDatabaseExist(dbName) {
+		logger.Fatal("quit due to create database [%s] fail!")
+	}
 
-	// db.PtrMysqlClient.DB.DB().Raw("SHOW DATABASES LIKE ?", "mysql").Scan(&showDatabaseRes)
-	// fmt.Println(util.ToString(showDatabaseRes))
+	logger.Info("create database [%s] successfully", dbName)
+}
 
+func isDatabaseExist(dbName string) bool {
+	var (
+		err error
+		res []map[string]string
+	)
+
+	// 检查数据库是否存在
+	// SQL 中预处理语句的参数化不能用于表名、列名和数据库名等, 这些名字在 SQL 执行前就需要确定下来
+	res, err = db.PtrMysqlClient.Query2StringMap(fmt.Sprintf("SHOW DATABASES like '%s';", dbName))
+	if err != nil {
+		logger.Fatal("show database [%s] fail with err [%v]", dbName, err)
+	}
+	if len(res) == 1 {
+		return true
+	}
+	return false
 }
