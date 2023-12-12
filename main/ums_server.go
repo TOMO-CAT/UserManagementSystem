b77ca05f182443373a6ee23a6bad2bf60f15fb72
zifeng.yang <zifeng.yang@zelostech.com>
2023-12-12 11:56:52 +0800

    [flags]: use client flags

--- main/ums_server.go
+++ main/ums_server.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"sync"
+	"syscall"
+	"time"
 
 	"github.com/TOMO-CAT/UserManagementSystem/pkg/config"
 	"github.com/TOMO-CAT/UserManagementSystem/pkg/db"
@@ -29,7 +31,7 @@ func main() {
 		&cli.StringFlag{
 			Name:  "conf",
 			Usage: "config file path",
-			Value: "conf/config.toml",
+			Value: "conf/config-dev.toml",
 		}, &cli.StringFlag{
 			Name:  "log-conf",
 			Usage: "log config file path",
@@ -40,20 +42,15 @@ func main() {
 
 func run(flags map[string]interface{}, ctx context.Context, errChan chan error, appWg *sync.WaitGroup) error {
 	// 初始化日志
-	var loggerConfPath string = kDefaultLoggerPath
-	if val, exists := flags["log-conf"]; exists {
-		loggerConfPath = fmt.Sprintf("%v", val)
-	}
+	var loggerConfPath = fmt.Sprintf("%v", flags["log-conf"])
 	if err := logger.InitLogger(loggerConfPath); err != nil {
-		fmt.Printf("init logger [%s] fail with err [%v]\n", loggerConfPath, err)
-		return fmt.Errorf("init logger [%s] fail with err [%v]", loggerConfPath, err)
+		errmsg := fmt.Sprintf("init logger [%s] fail with err [%v]", loggerConfPath, err)
+		fmt.Printf("%s\n", errmsg)
+		return fmt.Errorf(errmsg)
 	}
 
 	// 初始化配置文件
-	var umsConfPath string = kDefaultUmsConfPath
-	if val, exists := flags["conf"]; exists {
-		umsConfPath = fmt.Sprintf("%v", val)
-	}
+	var umsConfPath = fmt.Sprintf("%v", flags["conf"])
 	if err := config.ParseConfig(umsConfPath); err != nil {
 		logger.Error("parse ums config [%s] fail with err [%v]", umsConfPath, err)
 		return err
@@ -76,5 +73,16 @@ func run(flags map[string]interface{}, ctx context.Context, errChan chan error, 
 
 	// 启动 grpc 服务
 	logger.Info("start grpc server with port [%d]", config.GlobalUmsConfig.GrpcServer.Port)
+
+	// 启动 5 秒后自动退出
+	go func() {
+		for i := 0; i < 5; i++ {
+			logger.Info("ums server is running...")
+			time.Sleep(time.Second * 1)
+		}
+		logger.Info("master service update, going to quit and restart!")
+		syscall.Kill(syscall.Getpid(), syscall.SIGINT)
+	}()
+
 	return grpcserver.NewServer().Start(ctx, config.GlobalUmsConfig.GrpcServer.Port)
 }
